9891f7e4bad51975056fb4ac373930ee88985671
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-23 04:32:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-Scooter with year, color, and model properties
-Driver with name, age, and experience properties
-PickupLocation with address and city properties
-
-
 function Scooter(year, color, model) {
     this.year = year;
     this.color = color;
